fa91a7aa9de1d3488b9a9ca20a2fbb064877980a
TrustedInstaller64 <xsy3110293296@outlook.com>
2026-07-18 23:46:37 +0800

    Initial Commit

---
+++ AMPHOREUS/AMPHOREUSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AMPHOREUSApp.swift
+//  AMPHOREUS
+//
+//  Created by TrustedInstaller on 2026/7/18.
+//
+
+import SwiftUI
+
+@main
+struct AMPHOREUSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AMPHOREUSTests/AMPHOREUSTests.swift
@@ -0,0 +1,19 @@
+//
+//  AMPHOREUSTests.swift
+//  AMPHOREUSTests
+//
+//  Created by TrustedInstaller on 2026/7/18.
+//
+
+import Testing
+@testable import AMPHOREUS
+
+struct AMPHOREUSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
